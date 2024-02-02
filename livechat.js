3dfe79bf54590207cdb8eaeb64e0080d438100d6
Salo200221 <158565398+Salo200221@users.noreply.github.com>
2024-02-02 20:55:01 +0100

    Update livechat.js

--- livechat.js
+++ livechat.js
@@ -1,6 +1,4 @@
-window.onload = function () {
-    console.log("Script loaded successfully.");
-}function checkPassword() {
+function checkPassword() {
     var enteredPassword = document.getElementById('password').value;
     if (enteredPassword === '2000174786') {
         showHiddenContent();
@@ -11,29 +9,4 @@ window.onload = function () {
 
 function showHiddenContent() {
     document.getElementById('hidden-content').style.display = 'block';
-    displayBinaryCode('The Matrix is a system, Neo.');
-    displayMessage('Nothing is as it seems, Philipp... Look around, what do you see?');
-}
-
-function displayBinaryCode(text) {
-    var binaryCodeElement = document.getElementById('binary-code');
-    var binaryCode = text.split('').map(function (char) {
-        return char.charCodeAt(0).toString(2);
-    }).join(' ');
-    binaryCodeElement.textContent = binaryCode;
-}
-
-function displayMessage(message) {
-    var messageElement = document.getElementById('message');
-    var index = 0;
-
-    function typeWriter() {
-        if (index < message.length) {
-            messageElement.innerHTML += message.charAt(index);
-            index++;
-            setTimeout(typeWriter, 50);
-        }
-    }
-
-    typeWriter();
 }
