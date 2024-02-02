bce3f390c77fd7b86489da452c22aacf2410a245
Salo200221 <158565398+Salo200221@users.noreply.github.com>
2024-02-02 19:55:44 +0100

    Update livechat.js

--- livechat.js
+++ livechat.js
@@ -1,39 +1,15 @@
 function checkPassword() {
-    var enteredPassword = document.getElementById('password').value;
-
-    if (enteredPassword === '2000174786') {
-        //<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/65bcdb238d261e1b5f5b4933/1hlli6afi';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
-        openLiveChat();
+    var password = document.getElementById("passwordInput").value;
+    
+    if (password === "2000174786") {
+        document.getElementById("chat-popup").style.display = "block";
     } else {
-        alert('Incorrect password. Try again.');
+        alert("Incorrect code. Try again.");
     }
 }
 
-function openLiveChat() {
-    // <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/65bcdb238d261e1b5f5b4933/1hlli6afi';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
-    alert('Live chat is now open!');
+function sendMessage() {
+    var message = document.getElementById("messageInput").value;
+    document.getElementById("chatArea").innerHTML += "<p>You: " + message + "</p>";
+    document.getElementById("messageInput").value = "";
 }
