852ed8b385b824970e5a8057ca5e1be5a4a846d2
Salo200221 <158565398+Salo200221@users.noreply.github.com>
2024-02-02 19:52:34 +0100

    Update livechat.js

--- livechat.js
+++ livechat.js
@@ -2,7 +2,19 @@ function checkPassword() {
     var enteredPassword = document.getElementById('password').value;
 
     if (enteredPassword === '2000174786') {
-        // If the password is correct, open the live chat
+        //<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/65bcdb238d261e1b5f5b4933/1hlli6afi';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
         openLiveChat();
     } else {
         alert('Incorrect password. Try again.');
