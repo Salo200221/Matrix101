a9725d9427a9485988a42fdd83e30a2ab713204d
Salo200221 <158565398+Salo200221@users.noreply.github.com>
2024-02-15 00:10:56 +0100

    Update livechat.js

--- livechat.js
+++ livechat.js
@@ -1,1 +1,12 @@
+function checkPassword() {
+    var enteredPassword = document.getElementById('password').value;
+    if (enteredPassword === '2000174786') {
+        showHiddenContent();
+    } else {
+        alert('Incorrect password. Try again.');
+    }
+}
 
+function showHiddenContent() {
+    document.getElementById('hidden-content').style.display = 'block';
+}
